4898a8865088d6da308749643bf395cb0eb375a4
Ali Akpinar <ali68_1905@icloud.com>
2024-01-24 11:20:45 +0100

    Initial Commit

---
+++ TravelNote/TravelNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelNoteApp.swift
+//  TravelNote
+//
+//  Created by Ali Akpinar on 24.01.24.
+//
+
+import SwiftUI
+
+@main
+struct TravelNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
